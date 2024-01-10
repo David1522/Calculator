8781840aac37a3a3e3c04535ad0591289d330f48
David J <139520081+David1522@users.noreply.github.com>
2024-01-10 07:06:31 -0500

    Calculator files, desing and styles added.

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+let firstNum;
+let operator;
+let secondNum;
+
+function operate () {
+  
+}
+
+function add () {
+
+}
+
+function subtract () {
+
+}
+
+function multiply () {
+
+}
+
+function divide () {
+
+}
